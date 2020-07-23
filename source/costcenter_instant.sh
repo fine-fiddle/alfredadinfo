b3c6b5f938353518f388d2e471edad79096ca728
jerome <jerome@jerome.jerome>
2020-07-23 15:34:51 -0500

    add quick cost center functionality

---
+++ source/costcenter_instant.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+query="$(pbpaste)";
+
+if [[ "$query" == *"@"* ]]; then
+  query=$(echo "$query" | awk 'BEGIN { FS = "@" } ; {print $1}');
+fi
+
+
+CC=$(dscl /Active\ Directory/${ADDOMAIN}/All\ Domains/ -read /Users/$query msExchExtensionAttribute16 | awk '{print $NF}');
+
+if [[ "$CC" == "dsRecTypeStandard:Users" ]]; then
+  echo "User Not Found.";
+  exit 1;
+fi
+
+if [[ "$CC" == "valid." ]]; then
+  echo "No connection to Active Directory.";
+  exit 1;
+fi
+
+echo "$CC";
+
+#bad user "dsRecTypeStandard:Users"
+#from "name: dsRecTypeStandard:Users"
+#bad connection: "valid."
+# from "Data source (/Active Directory/PAYPALCORP/All Domains/) is not valid."
