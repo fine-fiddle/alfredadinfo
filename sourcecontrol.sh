c8817af0fe88fdb9da1f0682d456a63980fc325d
jerome <jerome@jerome.jerome>
2020-07-23 16:04:53 -0500

    add script for source control, fix package style

---
+++ sourcecontrol.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+#your workflow directory path goes here.
+WORKFLOWPATH=""
+
+echo "Clearing out old source files. modify this script if you're tired of confirming each";
+
+rm -i -r ./source/*
+
+echo "Copying your live workflow files to the source directory";
+cp -r "$WORKFLOWPATH"/* ./source/
+
+echo "removing old .alfredworkflow";
+rm AD\ Info.alfredworkflow
+
+echo "Building new .alfredworkflow from the new source files";
+zip -jFS AD\ Info.alfredworkflow source/*
